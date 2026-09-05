24818c3cef57e17b115fc2bcf797cbbe67a9bc53
IanYuan00 <yuangao2517@gmail.com>
2026-09-04 20:26:50 -0400

    Add input validation and error handling

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(cors());
 
 app.post('/applications', async (req, res) => {
     const { company, position, status, date, notes } = req.body;
+    if (!company || !position) {
+        return res.status(400).json({ error: 'Company and position are required.' })
+    }
     const newApplication = await prisma.application.create({
         data: { company, position, status, date, notes }
     });
@@ -29,15 +32,31 @@ app.get('/applications', async (req, res) => {
 
 app.delete('/applications/:id', async (req, res) => {
     const { id } = req.params;
-    await prisma.application.delete({ where: { id } });
-    res.json({ message: 'Deleted successfully' });
+    try {
+        await prisma.application.delete({ where: { id } });
+        res.json({ message: 'Deleted successfully' });
+    } catch (error) {
+        res.status(404).json({ error: 'Application not found.' });
+    }
+
 })
 
 app.patch('/applications/:id', async (req, res) => {
     const { id } = req.params;
-    const updatedApplication = await prisma.application.update({
-        where: { id },
-        data: req.body
-    });
-    res.json(updatedApplication);
+    if ('company' in req.body && req.body.company === '') {
+        return res.status(400).json({ error: 'Company cannot be empty.' });
+    }
+    if ('position' in req.body && req.body.position === '') {
+        return res.status(400).json({ error: 'Position cannot be empty.' })
+    }
+    try {
+        const updatedApplication = await prisma.application.update({
+            where: { id },
+            data: req.body
+        });
+        res.json(updatedApplication);
+    } catch (error) {
+        res.status(404).json({ error: 'Application no longer exist.' })
+    }
+
 })
